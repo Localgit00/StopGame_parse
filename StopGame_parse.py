2dcf1e70366baa78d1b4f9f33efafafe140c7eb2
Gitrx70 <53248000+Gitrx70@users.noreply.github.com>
2020-10-11 20:01:14 +0300

    Update StopGame_parse.py

--- StopGame_parse.py
+++ StopGame_parse.py
@@ -6,12 +6,16 @@ pages=[]
 def get_review():
     for x in range(1, max_page+1):
         pages.append(requests.get('https://stopgame.ru/review/new/stopchoice/p' + str(x)))
+    for page in pages:
+        soup = BS(page.text, features="lxml")
 
-    for r in pages:
-        html = BS(r.content, 'html.parser')
-        for el in html.select('.lent-block'):
-            title = el.select('.lent-title>a')
-            print(title[0].text)
-        
+        element=soup.select('body > div.page-layout > div > div > div > section:nth-child(4) > div') 
+
+        for div in element:
+            soup = BS(div.text, features="lxml")
+            div=div.select('div.article-description > div.caption.caption-bold > a')
+            for reviw in div:
+                print(reviw.text)
+            
 if __name__ == '__main__':
     get_review()
